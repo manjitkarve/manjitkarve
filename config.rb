1c8bebd1f47f798a84e4b6ee7f439e70ea1f0f24
Manjit Karve <manjit.karve@gmail.com>
2016-06-26 20:14:52 +0530

    Don't generate map by default

--- config.rb
+++ config.rb
@@ -7,5 +7,4 @@ javascripts_dir = 'assets/js'
 relative_assets = true
 line_comments = true
 output_style = (environment == :production) ? :compressed : :expanded
-sourcemap = true
 # output_style = :compressed
